6b80e0dc8b601d3f882a205873b3a180aa662b24
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-02 15:04:35 -0800

    add Axios and useEffect in imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import TaskList from './components/TaskList';
 
