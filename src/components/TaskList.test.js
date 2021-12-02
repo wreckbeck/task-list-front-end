1d671a17abca44d0753c1da9c85f26c57bb92f3d
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-02 15:04:05 -0800

    fix props in TaskList.test.js

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -21,8 +21,8 @@ describe('TaskList Renders', () => {
     render(
       <TaskList
         tasks={tasks}
-        onTaskClickCallback={() => {}}
-        onTaskDeleteCallback={() => {}}
+        onToggleCompleteCallback={() => {}}
+        onDeleteCallback={() => {}}
       />
     );
 
