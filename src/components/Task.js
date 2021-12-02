c44caf4083d518eadd0cdce7bae600e4abd35570
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-02 14:31:23 -0800

    lift state up in Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,21 +5,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './Task.css';
 
-const Task = ({ id, text, done }) => {
-  const [isDone, setIsDone] = useState(done);
-  const buttonClass = isDone ? 'tasks__item__toggle--completed' : '';
+const Task = ({
+  id,
+  text,
+  done,
+  onToggleCompleteCallback,
+  onDeleteCallback,
+}) => {
+  const buttonClass = done ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setIsDone(!isDone)}
+        onClick={() => onToggleCompleteCallback(id)}
       >
         {text}
       </button>
       <button
         className="tasks__item__remove button alert pull-right"
         data-testid={`delete button ${id}`}
+        onClick={() => onDeleteCallback(id)}
       >
         <i className="fa fa-times">
           <FontAwesomeIcon icon={faTimes} />
@@ -33,6 +39,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   done: PropTypes.bool.isRequired,
+  onToggleCompleteCallback: PropTypes.func.isRequired,
+  onDeleteCallback: PropTypes.func.isRequired,
 };
 
 export default Task;
